40bac258ec62603a68e9d2b3390a973201c32e63
Will Reynolds <william_reynolds@apple.com>
2018-02-02 16:58:22 -0600

    Added Fixtures
    Doctrine Fixtures added via composer.

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -14,6 +14,7 @@ class AppKernel extends Kernel
             new Symfony\Bundle\MonologBundle\MonologBundle(),
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
+            new \Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
             new MyBundle\MyBundle(),
@@ -46,6 +47,10 @@ class AppKernel extends Kernel
 
     public function registerContainerConfiguration(LoaderInterface $loader)
     {
-        $loader->load($this->getRootDir().'/config/config_'.$this->getEnvironment().'.yml');
+        try {
+            $loader->load($this->getRootDir() . '/config/config_' . $this->getEnvironment() . '.yml');
+        } catch (Exception $e) {
+            die("Container config registration raised an exception");
+        }
     }
 }

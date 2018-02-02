2020375d96dc12bdf101241e701d489513b0e4f1
Will Reynolds <william_reynolds@apple.com>
2018-02-02 15:37:34 -0600

    Create AppBundle aboutAction
    Created about action and generated a twig file which uses conditional
    branching. It also leverages route defaults.

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -4,18 +4,29 @@ namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Request;
+
 
 class DefaultController extends Controller
 {
     /**
      * @Route("/", name="homepage")
      */
-    public function indexAction(Request $request)
+    public function indexAction()
     {
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
         ]);
+    }
+
+    /**
+     * @Route("/about/{name}", name="about", defaults={"name":null})
+     *
+     * @param $name - The name to be passed to the controller
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function aboutAction($name)
+    {
+        return $this->render('about/index.html.twig', array('name' => $name));
     }
 }
